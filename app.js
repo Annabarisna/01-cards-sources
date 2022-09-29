b68b9347e5063953f1e74266589c4d8bcee01aaf
AnnaLvova <annlvova.7th@gmail.com>
2022-09-29 18:54:20 +0700

    test function with random slide

--- app.js
+++ app.js
@@ -20,6 +20,21 @@
 
 //slidesPlugin(random, slides[random]);
 
+function slidesPlugin(ranSlide){
 const slides = document.querySelectorAll('.slide'); //находим слайды
 const ranSlide = Math.floor(Math.random() * slides.length); //получаем случайный слайд
 ranSlide.classList.add('active'); //активируем случайный слайд
+for (const slide of slides) {
+    slide.addEventListener('click', () => {
+      clearActiveClasses()
+             slide.classList.add('active')
+       })
+     }
+ 
+    function clearActiveClasses () {
+         slides.forEach((slide) => {
+             slide.classList.remove('active')
+         }) 
+     }
+ }
+ slidesPlugin();
